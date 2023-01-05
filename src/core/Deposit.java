3ea8b10830638e408daf147a52235f090b00903c
Victor <111821743+victordarii@users.noreply.github.com>
2023-01-05 15:05:37 -0600

    Create Deposit.java

---
+++ src/core/Deposit.java
@@ -0,0 +1,9 @@
+package core;
+
+public class Deposit {
+  // for depositing
+  
+  // created on github for practicing
+  
+  // this is to intencionally create conflict because it's created in the wrong repo, instead of feature-a it's created in developer branch
+}
